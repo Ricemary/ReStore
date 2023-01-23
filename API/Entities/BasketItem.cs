e8a44c81d81f9bd63d97703b62da4151a488e184
Ricemary <ha-training9527@outlook.com>
2023-01-23 11:13:48 +0800

    End of section 6

---
+++ API/Entities/BasketItem.cs
@@ -0,0 +1,20 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace API.Entities
+{
+    [Table("BasketItems")]
+    public class BasketItem
+    {
+        public int id { get; set; }
+        public int Quantity { get; set; }
+
+        //navigation properites
+        public int ProductId { get; set; }
+        public Product Product { get; set; }
+
+        public int BasketId { get; set; }
+
+        public Basket Basket { get; set; }
+
+    }
+}
